c5dfc412d054b12e63207b5a746ebd9d8f79da2d
Siprahak <cyanide455@gmail.com>
2024-06-19 03:02:28 +0700

    Update tentang-kami.php

--- tentang-kami.php
+++ tentang-kami.php
@@ -8,59 +8,73 @@
     <!-- Bootstrap -->
     <link rel="stylesheet" href="bootstrap/bootstrap-5.2.3-dist/css/bootstrap.min.css">
 
+    <!-- FontAwesome -->
     <link rel="stylesheet" href="fontawesome/fontawesome-free-6.5.2-web/css/fontawesome.min.css">
 
-
-     <!-- CSS -->
-      <link rel="stylesheet" href="css/style.css">
-
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <style>
-    p{
+    p {
         font-size: 20px;
     }
 
+    .banner2 {
+        background-image: url('image/banner2.jpeg');
+        background-size: cover;
+        height: 400px;
+    }
+
+    .banner2 h1 {
+        font-size: 3rem;
+    }
+
+    .section-title {
+        margin-top: 2rem;
+        margin-bottom: 1.5rem;
+    }
 </style>
+
 <body>
-    <?php require "navbar.php"?>
+    <?php require "navbar.php"; ?>
 
     <!-- Banner -->
-    <div class="container-fluid banner2 d-flex align-items-center" style="background-image: url('image/banner2.jpeg'); background-size: cover; height: 400px;">
-        <div class="container">
-            <h1 class="text-white text-center fs-1">Tentang Desa Kami</h1>
-        </div>
-    </div>
-    
-    <div class="container py-5  text-center">
-        <h1 class="mb-4">Sejarah Desa</h1>
-        <p>Mungkin nama Desa Parengan terlalu asing didengar di telinga masyarakat luas. Tapi semua itu tidak berlaku pada masyarakat yang berada di Kabupaten Lamongan. Hal tersebut dikarenakan Lokasi desa Parengan berada di Kota Lamongan atau lebih tepatnya berada di Kecamatan Maduran.
-        <br>
-        Nama Parengan sendiri diambil dari cerita terkenal “MBOK RONDO DADAPAN” seorang putri yang sangat elok dan disegani oleh rakyatnya. Pada saat mengadakan perjalanan beliau tak sengaja singgah di sebuah desa asing. Sang putrid merasa sangat senang dengan masyarakat di desa tersebut. Hal tersebut dikarenakan masyarakat tersebut amat senang membantu dan memberikan apa yang mereka miliki pada pendatang termasuk sang putri. Sang orang-orang yang Putri temui itu sangant peduli dengan sesama termasuk padanya. Sehingga sang putrid menyebut mereka dengan “PEPARENG” yang artinya selalu ingin memberi. Dan atas segala kebaikan masyarakat, hingga kini di desa tersebut dikenal dengan sebutan Desa “PARENGAN”.
-        </p>
-        <br><br>
-        <h1 class="mb-4 mt-7">Berita Desa</h1>
-        <p>Desa Parengan yang berada di Kecamatan Maduran, Kabupaten Lamongan, Provinsi Jawa Timur ini merupakan desa salah satu desa yang memiliki produk unggulan yaitu kain tenun ikatnya. Selain diekspor ke luar negeri, kain tenun ikat Parengan juga sudah dipakai orang-orang terkenal yang ada di Indonesia.
-        <br>
-        Disampaikan Kadis Disperindag melalui Kabag Protokol dan Komunikasi Pimpinan Arif Bakhtiar, sentra industri tenun ikat parengan sejauh ini telah menjangkau pasar luar negeri dengan nilai ekspor mencapai Rp 17.755.000.000.
-        <br>
-        “Sentra industri tenun ikat parengan di tahun 2021 telah menerbangkan 4.600 kodi sarung tenun ikat yang menjangkau berbagai negara di Timur Tengah serta Afrika. Di Tahun 2021 telah  diekspor yakni sarung tenun ikat sebanyak 4.600 kodi dengan nilai ekspor Rp. 17.755.000.000,” pungkas Arif.
-        <br>
-        Gubernur Jawa Timur, Khofifah Indar Parawansah, mengusulkan Desa Parengan untuk menjadi desa Devisa agar daya saing produk unggulannya dapat lebih luas dan meningkat. </p>
+    <div class="container-fluid banner2 d-flex align-items-center justify-content-center text-center">
+        <h1 class="text-white">Tentang Desa Kami</h1>
     </div>
 
-     <!-- Footer -->
+    <!-- Content -->
+    <div class="container py-5 text-center">
+        <h1 class="section-title">Sejarah Desa</h1>
+        <p>
+            Mungkin nama Desa Parengan terlalu asing didengar di telinga masyarakat luas. Tapi semua itu tidak berlaku pada masyarakat yang berada di Kabupaten Lamongan. Hal tersebut dikarenakan Lokasi desa Parengan berada di Kota Lamongan atau lebih tepatnya berada di Kecamatan Maduran.
+            <br><br>
+            Nama Parengan sendiri diambil dari cerita terkenal “MBOK RONDO DADAPAN” seorang putri yang sangat elok dan disegani oleh rakyatnya. Pada saat mengadakan perjalanan beliau tak sengaja singgah di sebuah desa asing. Sang putri merasa sangat senang dengan masyarakat di desa tersebut. Hal tersebut dikarenakan masyarakat tersebut amat senang membantu dan memberikan apa yang mereka miliki pada pendatang termasuk sang putri. Orang-orang yang Putri temui itu sangat peduli dengan sesama termasuk padanya. Sehingga sang putri menyebut mereka dengan “PEPARENG” yang artinya selalu ingin memberi. Dan atas segala kebaikan masyarakat, hingga kini di desa tersebut dikenal dengan sebutan Desa “PARENGAN”.
+        </p>
+
+        <h1 class="section-title">Berita Desa</h1>
+        <p>
+            Desa Parengan yang berada di Kecamatan Maduran, Kabupaten Lamongan, Provinsi Jawa Timur ini merupakan desa salah satu desa yang memiliki produk unggulan yaitu kain tenun ikatnya. Selain diekspor ke luar negeri, kain tenun ikat Parengan juga sudah dipakai orang-orang terkenal yang ada di Indonesia.
+            <br><br>
+            Disampaikan Kadis Disperindag melalui Kabag Protokol dan Komunikasi Pimpinan Arif Bakhtiar, sentra industri tenun ikat parengan sejauh ini telah menjangkau pasar luar negeri dengan nilai ekspor mencapai Rp 17.755.000.000.
+            <br><br>
+            “Sentra industri tenun ikat parengan di tahun 2021 telah menerbangkan 4.600 kodi sarung tenun ikat yang menjangkau berbagai negara di Timur Tengah serta Afrika. Di Tahun 2021 telah diekspor yakni sarung tenun ikat sebanyak 4.600 kodi dengan nilai ekspor Rp. 17.755.000.000,” pungkas Arif.
+            <br><br>
+            Gubernur Jawa Timur, Khofifah Indar Parawansah, mengusulkan Desa Parengan untuk menjadi desa Devisa agar daya saing produk unggulannya dapat lebih luas dan meningkat.
+        </p>
+    </div>
+
+    <!-- Footer -->
     <div class="container-fluid py-3 bg-dark text-light">
         <div class="container d-flex justify-content-between">
             <label>&copy; 2024 Parengan Store</label>
-            <label>Created By Kelompok 10  |  Parallel D</label>
+            <label>Created By Kelompok 10 | Parallel D</label>
         </div>
-    </div>   
     </div>
-    
 
-<script src="bootstrap/bootstrap-5.2.3-dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Scripts -->
+    <script src="bootstrap/bootstrap-5.2.3-dist/js/bootstrap.bundle.min.js"></script>
     <script src="fontawesome/fontawesome-free-6.5.2-web/js/all.min.js"></script>
-
 </body>
-</html>l
+</html>
